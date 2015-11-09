14af546a8bc1a46f3dc4315098811fc781adb0c1
Sam Starling <mail@samstarling.co.uk>
2015-11-09 22:29:21 +0100

    Push a random number every 2 seconds

--- push_data.sh
+++ push_data.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-echo "metric_name 12345" | curl --data-binary @- http://192.168.99.100:9091/metrics/job/job_name -v
+while true; do
+  echo "metric_name $RANDOM" | curl --data-binary @- http://192.168.99.100:9091/metrics/job/job_name -v
+  sleep 2
+done
